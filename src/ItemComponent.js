bc6466cf54d127d9cede0b180356084f3332a973
danielkauffmann <danielvenna2@gmail.com>
2020-08-21 15:30:41 -0300

    Listing item of each list

--- src/ItemComponent.js
+++ src/ItemComponent.js
@@ -2,5 +2,8 @@ import React from "react";
 
 
 export default function ItemComponent(props){
-    return <li>Item desc.: { props.name } </li>
+    const status = props.status
+    return <li>Item desc.: { props.name }
+        <p>Status: {status ? <div>Pago</div> : <div>Não Pago</div>} </p>
+    </li>
 }
